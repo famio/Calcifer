53bb02b400a20b4cb00de5b0c2beb0e9c669e711
famio <smartboy.famio@gmail.com>
2021-10-31 15:03:03 +0900

    Project作成

---
+++ Calcifer/CalciferApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalciferApp.swift
+//  Calcifer
+//
+//  Created by chalingco on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct CalciferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
